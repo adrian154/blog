79e9857bd2e1f37238234364cf2897496564a9ba
adrian154 <94568a@gmail.com>
2023-04-19 00:03:47 -0700

    harmonics

--- public/blogposts/tuning/blogpost.md
+++ public/blogposts/tuning/blogpost.md
@@ -1,8 +1,8 @@
-You can tuna fish, but you can't tuna piano. Wait, what?
+You can tuna fish, but you can't tune a piano. Wait, what?
 
 Okay, that's not quite true. You *can* tune a piano, but only approximately. It turns out that the mathematics behind the 12-tone scale used in most Western music hides some sinister secrets.
 
-Let's start with the basics. Behind every key on a piano are 1&ndash;3 strings, which have been tensioned so that when the strings are struck they will ring out at a specific frequency. Each note is associated with a distinct frequency, and keys are arranged such that the frequency (pitch) of the notes increases from left to right.
+Let's start with the basics. Behind every key on a piano are 1&ndash;3 strings, which have been tensioned so that when the strings are struck they will ring out at a specific frequency. Each note is associated with a distinct frequency, and keys are arranged such that the pitch of the notes increases from left to right.
 
 Of course, the burning question remains: how do we know what frequency to assign to each note? The answer to this is pretty nuanced, but luckily we get one freebie. Every tuning system starts by assigning a specific frequency to an arbitrary key to use as a reference point; we call this the pitch standard. The most common pitch standard in modern times is A440, which sets the frequency of the first A above middle C to 440 Hz.
 
@@ -10,11 +10,11 @@ Now, let's do a little experiment. This is what it sounds like if I play A<sub>4
 
 <audio controls src="a4.mp3"></audio>
 
-Ironically, my piano is pretty badly out of tune, but that doesn't matter for this demonstration. I can use [a bit of math](https://en.wikipedia.org/wiki/Fourier_transform) to take the sound and dissect it into the individual frequencies that make it up, kind of like how a prism can split light into its color components:
+Ironically, my piano is pretty badly out of tune, but that doesn't matter for this demonstration. I can use [a bit of math](https://en.wikipedia.org/wiki/Fourier_transform) to take the sound and dissect it into the individual frequencies that make it up, kind of like how a prism can split light into its color components. Here is the resulting spectrum:
 
 <figure>
     <img src="spectrum.png" alt="spectral power distribution of piano note">
-    <figcaption>Disclaimer: the y-axis is a log scale. In reality, the harmonics after the first one are a lot weaker.</figcaption>
+    <figcaption>Disclaimer: the y-axis is a log scale.</figcaption>
 </figure>
 
 There's a big peak representing the main frequency, but there are also a bunch of extra peaks, which are known as overtones or partials. The reason for their production has to do with the physics of string vibration. Essentially, a string that is fixed at both ends is limited to vibrating at integer multiples of its fundamental frequency; these patterns of vibration are called *resonant modes*. Together, these modes form the harmonic series.
@@ -23,7 +23,14 @@ When a string is struck or plucked, the resulting vibration contains multiple of
 
 TODO-Explain relationship between intervals and harmonic series.
 
-<canvas id="harmonics" width="700" height="700"></canvas>
+<canvas id="harmonics" width="640" height="500"></canvas>
+
 <button onclick="play()">Play</button>
 
 <script src="main.js"></script>
+
+# Further Reading (Watching?)
+
+https://www.youtube.com/watch?v=TYhPAbsIqA8
+
+https://www.youtube.com/watch?v=1Hqm0dYKUx4
--- public/blogposts/tuning/main.js
+++ public/blogposts/tuning/main.js
@@ -3,25 +3,45 @@ const canvas = document.getElementById("harmonics"),
 
 const audioCtx = new AudioContext();
 
-canvas.style.border = "1px solid black";
-const draw = () => {
+const PALETTE = ["#e3342f", "#f6993f", "#ffed4a", "#38c172", "#4dc0b5", "#3490dc", "#6574cd", "#9561e2", "#f66d9b"];
+const INTERVALS = ["octave", "perfect fifth", "perfect fourth", "major third", "minor third", "(...)", "(...)", "major second"];
+const draw = harmonic => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.font = "24px Arial";
     for(let i = 1; i <= 9; i++) {
-        ctx.fillText(i, 20, 15 + i * 50);
+        if(i == harmonic || i == harmonic + 1)
+            ctx.strokeStyle = PALETTE[i - 1];
+        else
+            ctx.strokeStyle = "#000000";
+        ctx.fillText(i, 25, i * 50 + 9);
         ctx.beginPath();
-        ctx.moveTo(60, 7 + i * 50);
-        for(let x = 0; x < 400; x++) {
-            ctx.lineTo(60 + x, 7 + i * 50 + Math.sin(x / 400 * i * Math.PI) * 12);
+        ctx.moveTo(60, i * 50);
+        for(let x = 0; x <= 400; x++) {
+            ctx.lineTo(60 + x, i * 50 + Math.sin(x / 400 * i * Math.PI) * 12);
         }
         ctx.stroke();
-        ctx.closePath();
         ctx.beginPath();
-        ctx.moveTo(60, 7 + i * 50);
-        for(let x = 0; x < 400; x++) {
-            ctx.lineTo(60 + x, 7 + i * 50 - Math.sin(x / 400 * i * Math.PI) * 12);
+        ctx.moveTo(60, i * 50);
+        for(let x = 0; x <= 400; x++) {
+            ctx.lineTo(60 + x, i * 50 - Math.sin(x / 400 * i * Math.PI) * 12);
         }
         ctx.stroke();
-        ctx.closePath();
+    }
+    for(let i = 0; i < 8; i++) {
+        if(i + 1 == harmonic) {
+            ctx.strokeStyle = "#ff0000";
+            ctx.lineWidth = 2;
+            ctx.fillStyle = "#ff0000";
+        } else {
+            ctx.strokeStyle = "#000000";
+            ctx.lineWidth = 1;
+            ctx.fillStyle = "#000000";
+        }
+        ctx.beginPath();
+        ctx.moveTo(470, i * 50 + 52);
+        ctx.bezierCurveTo(480, i * 50 + 75, 480, i * 50 + 75, 470, i * 50 + 98);
+        ctx.stroke();
+        ctx.fillText(INTERVALS[i], 490, (i + 1) * 50 + 34);
     }
 };
 
@@ -30,9 +50,9 @@ const playNote = (freq, length, attack) => {
     const envelope = audioCtx.createGain();
     const time = audioCtx.currentTime;
     envelope.gain.setValueAtTime(0, time);
-    envelope.gain.linearRampToValueAtTime(0.5, time + attack);
+    envelope.gain.linearRampToValueAtTime(0.3, time + attack);
     setTimeout(() => {
-        envelope.gain.setValueAtTime(0.5, audioCtx.currentTime);
+        envelope.gain.setValueAtTime(0.3, audioCtx.currentTime);
         envelope.gain.linearRampToValueAtTime(0, audioCtx.currentTime + attack);
     }, 1000 * (length - attack));
     envelope.connect(audioCtx.destination);
@@ -47,8 +67,25 @@ const playNote = (freq, length, attack) => {
 
 const play = () => {
     audioCtx.resume();
-    playNote(440*9/8, 0.75, 0.1);
-    setTimeout(() => playNote(440*9/8*9/8, 0.5, 0.1), 650);
+    let harmonic = 1;
+    const run = () => {
+        draw(harmonic, harmonic + 1);
+        playNote(110 * harmonic, 0.75, 0.1);
+        setTimeout(() => {
+            playNote(110 * (harmonic + 1), 0.75, 0.1);
+            setTimeout(() => {
+                playNote(110 * harmonic, 0.5, 0.1);
+                playNote(110 * (harmonic + 1), 0.5, 0.1);
+                harmonic++;
+                if(harmonic < 9) {
+                    setTimeout(run, 600);
+                } else {
+                    setTimeout(draw, 600);
+                }
+            }, 750);
+        }, 650);
+    };
+    run();
 };
 
 draw();
